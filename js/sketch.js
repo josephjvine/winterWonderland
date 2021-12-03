0adfe133b88f9648c38c3d65a0b02ad7d5f60198
josephjvine <93606160+josephjvine@users.noreply.github.com>
2021-12-03 17:02:55 +0000

    End of session upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,54 @@
+var img;
+
+const snowFlakeCount = 1500;
+const size = 5;
+const gravity = 0.5;
+
+var snowFlakes = [];
+
+function preload() {
+    img = loadImage('../img/bg.jpg')
+}
+
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(1280, 720);
     canvas.parent("p5container");
+
+    
+
+    for(let i = 0; i < snowFlakeCount;i++){
+
+        snowFlakes.push({
+            x: random(width),
+            y: random(height)
+        });
+
+        console.log(snowFlakes);
+
+    }
+
+
 }
 
 function draw() {
-    background(220);
 
+    image(img, 0, 0, width, height);
+
+    noStroke();
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+
+    for(let i = 0; i < snowFlakes.length; i++){
+        var snowflake = snowFlakes[i];
+
+    ellipse(snowflake.x, snowflake.y, size);
+
+    if(snowflake.y > height + size){
+        snowflake.y = -size;   
+       } else {
+        snowflake.y += gravity;
+       }
+
+    }
+
 }
